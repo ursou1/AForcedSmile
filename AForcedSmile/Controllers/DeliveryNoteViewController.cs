f7511fd42c26b4e8b8332dee29701b3916fa7d60
Soul <xorekvblendere@mail.ru>
2022-03-29 20:19:04 +1000

    123

---
+++ AForcedSmile/Controllers/DeliveryNoteViewController.cs
@@ -0,0 +1,44 @@
+﻿using AForcedSmile.db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AForcedSmile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryNoteViewController : ControllerBase
+    {
+        //dont forget
+        private readonly ForcedSmileContext dbContext;
+        public DeliveryNoteViewController(ForcedSmileContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //dont forget
+
+        //[HttpGet]
+        //public IEnumerable<DeliveryNoteApi> Get()
+        //{
+        //    return dbContext.DeliveryNotes.ToList().Select(s =>
+        //    {
+        //      return (DeliveryNoteApi)s;
+        //    });
+        //    return dbContext.DeliveryNotes.ToList().Select(s =>
+        //    {
+        //        return CreateDeliveryNoteApi((DeliveryNoteApi)s);
+        //    });
+        //}
+
+        private DeliveryNoteView CreateDeliveryNoteApi(DeliveryNote deliveryNote)
+        {
+            var result = new DeliveryNoteView { DeliveryNote = (DeliveryNoteApi)deliveryNote };
+            result.Provider = (ProviderApi)dbContext.Providers.First(s => s.Id == deliveryNote.ProviderId);
+            return result;
+        }
+    }
+}
--- ModelsApi/DeliveryNoteView.cs
+++ ModelsApi/DeliveryNoteView.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace ModelsApi
 {
-    public class DepartNoteApi: ApiBaseType
+    public class DeliveryNoteView
     {
-        public int Number { get; set; }
-        public DateTime DepartDate { get; set; }
-        public int ClientId { get; set; }
+        public DeliveryNoteApi DeliveryNote { get; set; }
         public ProviderApi Provider { get; set; }
     }
 }
