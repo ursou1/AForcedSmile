2fe9d22a3c2c7d3bcab99a22cf468d4bc5c6deb9
Soul <xorekvblendere@mail.ru>
2022-03-30 15:40:08 +1000

    add provider in delnote

--- ModelsApi/DeliveryNoteApi.cs
+++ ModelsApi/DeliveryNoteApi.cs
@@ -11,5 +11,6 @@ namespace ModelsApi
         public int Number { get; set; }
         public DateTime DeliveryDate { get; set; }
         public int ProviderId { get; set; }
+        public ProviderApi Provider { get; set; }
     }
 }
