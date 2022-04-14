378da741cf4ab1ba5ffed05e07b0acd66e8fa36e
Soul <xorekvblendere@mail.ru>
2022-04-14 20:47:21 +1000

    api addons add

---
+++ ModelsApi/SoftDeleteApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsApi
+{
+    public class SoftDeleteApi: ApiBaseType
+    {
+        public bool? Deleted { get; set; }
+        public DateTime? DeleteDate { get; set; }
+    }
+}
